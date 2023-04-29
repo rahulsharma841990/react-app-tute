8cb83459099bb5132beb04e835e0db31487b7f5e
rahulsharma841990 <rahulsharma841990@outlook.com>
2023-04-29 18:22:17 +0530

    7 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+// import './App.css';
+
+interface Record {
+  name: string;
+  email: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [row, setRow] = useState([]);
+
+  const createRow = () => {
+    const singleData: Record = {
+      name: 'Test',
+      email: 'test@gmail.com',
+    };
+    setRow((prevItems) => [...prevItems, singleData]);
+  };
+
+  const singleRow = () => {
+    return row.map((singleRecord: Record, index) => {
+      return (
+        <tr key={index}>
+          <td>{index + 1}</td>
+          <td>{singleRecord.name}</td>
+          <td>{singleRecord.email}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Row>
+          <Col className="bg-primary">Here is code</Col>
+          <Col className="bg-info">Here is code</Col>
+        </Row>
+        <Row>
+          <Col className="mt-5">
+            <Button onClick={createRow}>Add Row</Button>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col>
+            <table className="table table-bordered table-stripped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>{singleRow()}</tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
