5e300eb2b7d52e03d3fe3e53120c18d71eb921ac
rahulsharma841990 <rahulsharma841990@outlook.com>
2023-05-02 02:38:44 +0530

    1 file modified

--- src/context/SampleContext.tsx
+++ src/context/SampleContext.tsx
@@ -1,11 +1,26 @@
-import { createContext, useState } from 'react';
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  useState,
+  SetStateAction,
+} from 'react';
 
-export const SampleContext = createContext({
+interface SampleProviderProps {
+  children: ReactNode;
+}
+
+interface SampleContextType {
+  sampleValue: string;
+  setSampleValue: Dispatch<SetStateAction<string>>;
+}
+
+export const SampleContext = createContext<SampleContextType>({
   sampleValue: '',
   setSampleValue: () => {},
 });
 
-export const SampleProvider = ({ children }) => {
+export const SampleProvider = ({ children }: SampleProviderProps) => {
   const [sampleValue, setSampleValue] = useState('');
   return (
     <SampleContext.Provider value={{ sampleValue, setSampleValue }}>
